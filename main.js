2027687f29fe727e96686c58052de775bb1433b7
Michel-Maia <programador.michel.maia@gmail.com>
2022-06-08 12:56:28 -0300

    commit
    abrir ao clicar no ícone

--- main.js
+++ main.js
@@ -13,3 +13,21 @@
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav.toggle')
 //console.log(toggle)
+
+
+
+for (const element of toggle) {
+    element.addEventListener('click', function() {
+        nav.classList.toggle('show')
+    })
+    
+}
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+    link.addEventListener('click', function () {
+        nav.classList.remove('show')
+    })
+}
+
