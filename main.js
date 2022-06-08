e2474df8eccc33835f15da39609b1757e0c50d54
Michel-Maia <programador.michel.maia@gmail.com>
2022-06-08 12:45:04 -0300

    commit
    exemplo de js, criação da const nav e const toggle

---
+++ main.js
@@ -0,0 +1,15 @@
+// char - caracteres , conjunto de caracteres - string 
+// pode usar " " '' ou ``
+// number - numeros 
+// 1234
+
+// boolean: true ou false
+// verdadeiro ou falso
+
+// estrutura de dados { propriedade: 'valor' }
+// function liquidificador(frutas) { alert(frutas)}
+// liquidificador('maca, banana')
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav.toggle')
+//console.log(toggle)
